4be90f86f98a0be99c8f0973f293c13cdc6e0a28
Sessiom <caufaglj2@newpaltz.edu>
2024-10-26 15:50:33 -0400

    Implement POST API for creating products

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const failure = 1;
+const Failure = 1;
 
 export const connectDB = async () => {
     try{
@@ -8,6 +8,6 @@ export const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(failure);
+        process.exit(Failure);
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,34 @@
 import express from 'express'
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js'
+import Product from "./models/product.js"
 
 dotenv.config();
-const app = express();
 
-app.get("/products", (req,res) =>{})
+const app = express();
+const Created = 201
+const Bad_Request = 400
+const Internal_Server_Error = 500
+
+app.use(express.json())
+
+app.post("/api/products", async (req,res) =>{
+    const product = req.body
+
+    if((!product.name) || (!product.price) || (!product.image)){
+        return res.status(Bad_Request).json({success:false, message: "Please fill out all fields"});
+    }
+
+    const newProduct = new Product(product)
+
+    try{
+        await newProduct.save();
+        res.status(Created).json({ success: true, data: newProduct});
+    } catch(error) {
+        console.error("Error in creating product:", error.message);
+        res.status(Internal_Server_Error).json ({ success: false, message: "Server Error"});
+    }
+})
 
 
 app.listen(5000, () =>{
