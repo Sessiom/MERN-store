1ea1936d5bf6e48c1a0e0d340e6709ecd80b9c00
Sessiom <caufaglj2@newpaltz.edu>
2024-10-26 14:32:41 -0400

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
 //const express = require('express')
 import express from 'express'
+import dotenv from "dotenv"
+import { connectDB } from './config/db.js'
+
+dotenv.config();
+const app = express();
+
+app.get("/products", (req,res) =>{})
+
+
+app.listen(5000, () =>{
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+})
+
+
