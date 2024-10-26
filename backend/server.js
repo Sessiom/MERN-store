5d6c0d8f2848f01834e618c794094d2d32284886
Sessiom <caufaglj2@newpaltz.edu>
2024-10-26 16:42:05 -0400

    Implemented DELETE API for deleting products

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,15 @@ import dotenv from "dotenv"
 import { connectDB } from './config/db.js'
 import Product from "./models/product.js"
 
-dotenv.config();
-
 const app = express();
 const Created = 201
+const Deleted = 200
 const Bad_Request = 400
+const Not_Found = 404
 const Internal_Server_Error = 500
+const Port = 5000
 
+dotenv.config();
 app.use(express.json())
 
 app.post("/api/products", async (req,res) =>{
@@ -31,10 +33,24 @@ app.post("/api/products", async (req,res) =>{
     }
 })
 
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params
+    try{
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if (!deletedProduct) {
+            return res.status(Not_Found).json({ success: false, message: "Product not found" });
+        }
 
-app.listen(5000, () =>{
+        res.status(Deleted).json({ success: true, message: "Product deleted"})
+        
+    } catch (error) {
+        res.status(Not_Found).json({ status: false, message: "Product not found"})
+    }
+})
+
+app.listen(Port, () =>{
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log(`Server started at http://localhost:${Port}`);
 })
 
 
