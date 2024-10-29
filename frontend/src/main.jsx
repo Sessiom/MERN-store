bdd788d5d6099a9b129eac7085bc724627df07ad
Joshua <jcaufaglione@gmail.com>
2024-10-28 21:32:14 -0400

    Moved the Provider outside of BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
-       <Provider>
+      <Provider> 
+       <BrowserRouter>
         <App />
-       </Provider>
-      </BrowserRouter>
+       </BrowserRouter>
+      </Provider>
   </StrictMode>,
 )
